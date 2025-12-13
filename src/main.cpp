b6f847f9ab5fcadefa27cebe28834255ef6c03b2
Simon Ashton <simon@smntic.dev>
2025-12-13 12:25:47 -0800

    Web server POC

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,92 @@
 #include "Arduino.h"
+#include <ESP8266WiFi.h>
+#include "secrets.h"
+
+const int STATUS_LED_PIN = D8;
+
+const char* ssid = WIFI_SSID;
+const char* password = WIFI_PASSWORD;
+
+const long timeoutTime = 2000;
+
+WiFiServer server(80);
+
+void setup() {
+    Serial.begin(9600);
+
+    pinMode(STATUS_LED_PIN, OUTPUT);
+    digitalWrite(STATUS_LED_PIN, LOW);
+
+    // Connect to Wi-Fi network with SSID and password
+    Serial.print("Connecting to ");
+    Serial.println(ssid);
+    WiFi.begin(ssid, password);
+    while (WiFi.status() != WL_CONNECTED) {
+        delay(500);
+        Serial.print(".");
+    }
+    // Print local IP address and start web server
+    Serial.println("");
+    Serial.println("WiFi connected.");
+    Serial.println("IP address: ");
+    Serial.println(WiFi.localIP());
+    server.begin();
+}
+
+String prepareHtmlPage() {
+    String htmlPage;
+    htmlPage.reserve(1024);               // prevent ram fragmentation
+    htmlPage = 
+        "HTTP/1.1 200 OK\r\n"
+        "Content-Type: text/html\r\n"
+        "Connection: close\r\n"  // the connection will be closed after completion of the response
+        "\r\n"
+        "<!DOCTYPE HTML>"
+        "<html>"
+        "<body>"
+        "<h1>ESP8266 Web Server</h1>"
+        "</body>"
+        "</html>"
+        "\r\n";
+    return htmlPage;
+}
+
+void loop(){
+    delay(20);
+    digitalWrite(STATUS_LED_PIN, WiFi.status() == WL_CONNECTED ? HIGH : LOW);
+
+    WiFiClient client = server.accept();
+    if (client) {
+        Serial.println("\nClient connected");
+        while (client.connected()) {
+            // read line by line what the client (web browser) is requesting
+            if (client.available()) {
+                String line = client.readStringUntil('\r');
+                Serial.print(line);
+                // wait for end of client's request, that is marked with an empty line
+                if (line.length() == 1 && line[0] == '\n') {
+                    client.println(prepareHtmlPage());
+                    break;
+                }
+            }
+        }
+
+        while (client.available()) {
+            client.read();
+        }
+
+        // close the connection:
+        client.stop();
+        Serial.println("Client disconnected");
+    }
+}
+
+/* temperature + humidity sensor part
 #include "DHT.h"
 
 const int DHTPIN = D7;
 #define DHTTYPE DHT22
 DHT dht(DHTPIN, DHTTYPE);
-
-const int STATUS_LED_PIN = D8;
 
 void setup() {
     pinMode(STATUS_LED_PIN, OUTPUT);
@@ -31,3 +112,4 @@ void loop() {
     Serial.print("°C ");
     Serial.println();
 }
+*/
